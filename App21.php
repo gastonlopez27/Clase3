54b3595d7b6fd01d0458469019e464f4f3a57afc
gastonlopez27 <adpgastonlopez@gmail.com>
2016-09-11 22:20:46 -0300

    dasdasd

---
+++ App21.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+		<title></title>
+</head>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <th>Columna 1</th>
+    <th>Columna 2</th> 
+    <th>Columna 3</th>
+  </tr>
+  <tr>
+    <td>A1</td>
+    <td>B1</td> 
+    <td>C1</td>
+  </tr>
+   <tr>
+    <td>A2</td>
+    <td>B2</td> 
+    <td>C2</td>
+  </tr>
+   <tr>
+    <td>A3</td>
+    <td>B3</td> 
+    <td>C3</td>
+  </tr>
+   <tr>
+    <td>A4</td>
+    <td>B4</td> 
+    <td>C4</td>
+  </tr>
+  <tr>
+    <td>A5</td>
+    <td>B5</td> 
+    <td>C5</td>
+  </tr>
+</table>
+
+
+</body>
+</html>
---
+++ App22.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+		<title></title>
+</head>
+<body>
+
+<table style="width:100%">
+  <tr bgcolor="#FFFFFF">
+    <th>Columna 1</th>
+    <th>Columna 2</th> 
+    <th>Columna 3</th>
+  </tr>
+  <tr bgcolor="#BDBDBD">
+    <td>A1</td>
+    <td>B1</td> 
+    <td>C1</td>
+  </tr>
+   <tr bgcolor="#FFFFFF">
+    <td>A2</td>
+    <td>B2</td> 
+    <td>C2</td>
+  </tr>
+   <tr bgcolor="#BDBDBD">
+    <td>A3</td>
+    <td>B3</td> 
+    <td>C3</td>
+  </tr>
+   <tr bgcolor="#FFFFFF">
+    <td>A4</td>
+    <td>B4</td> 
+    <td>C4</td>
+  </tr>
+  <tr bgcolor="#BDBDBD">
+    <td>A5</td>
+    <td>B5</td> 
+    <td>C5</td>
+  </tr>
+</table>
+
+
+</body>
+</html>
